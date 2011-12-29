b01904d50e0d05f6ffada38269d069718a67e68f
Ilan Schnell <ischnell@enthought.com>
2011-12-29 00:15:43 -0600

    add Python 3.2 to classifiers

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
         "Topic :: Utilities",
     ],
     description = "efficient arrays of booleans -- C extension",
