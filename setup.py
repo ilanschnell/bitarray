d7ac4bc33c57e0dab78aff7cd60db6611aae98de
Ilan Schnell <ischnell@enthought.com>
2012-04-04 09:08:34 -0500

    ENH: simplified how setuptools import is handled

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import re
 from os.path import join
-from distutils.core import setup, Extension
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
 
 
 kwds = {}
