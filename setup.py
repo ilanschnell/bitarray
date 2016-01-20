0f23e8affa75518b3fbcfff7867d2dae7b41a8fd
Ilan Schnell <ilanschnell@gmail.com>
2016-01-20 09:38:11 -0600

    add official support for Python 3.5

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Topic :: Utilities",
     ],
     description = "efficient arrays of booleans -- C extension",
