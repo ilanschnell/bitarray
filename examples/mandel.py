99affd46980777402cfb9214d6d028b3a489c332
Ilan Schnell <ilanschnell@gmail.com>
2019-11-28 10:20:53 -0600

    examples: use numba for Mandelbrot

--- examples/mandel.py
+++ examples/mandel.py
@@ -1,53 +1,22 @@
-import os
 import sys
-import ctypes
-from subprocess import call
 from bitarray import bitarray
+from numba import jit
 
 width, height = 4000, 3000
+maxdepth = 500
 
 
-def compile_mandel():
-    c_file = 'tmp.c'
-    o_file = 'tmp.o'
-    so_file = 'tmp.so'
-
-    with open(c_file, 'w') as fo:
-        fo.write('''
-#define D 1001
-int mandel(double cr, double ci)
-{
-    int d = 1;
-    double zr = cr, zi = ci, zr2, zi2;
-    while (1) {
-        zr2 = zr * zr;
-        zi2 = zi * zi;
-        if (zr2+zi2 > 16.0)
-            break;
-        if (++d == D)
-            break;
-        zi = 2.0 * zr * zi + ci;
-        zr = zr2 - zi2 + cr;
-    }
-    return d;
-}
-''')
-    call(['gcc', '-c', '-O3', '-fpic', c_file])
-    call(['gcc', '-shared', '-o', so_file, o_file])
-    os.unlink(c_file)
-    os.unlink(o_file)
-
-    sobj = ctypes.CDLL(so_file)
-    f = sobj.mandel
-    f.argtypes = [ctypes.c_double, ctypes.c_double]
-    f.restype = ctypes.c_int
-    os.unlink(so_file)
-    return f
+@jit(nopython=True)
+def mandel(c):
+    d = 0
+    z = c
+    while abs(z) < 4.0 and d <= maxdepth:
+        d += 1
+        z = z * z + c
+    return d
 
 
 def main():
-    mandel = compile_mandel()
-
     data = bitarray(endian='big')
 
     for j in range(height):
@@ -56,11 +25,11 @@ def main():
         y = +1.5 - 3.0 * j / height
         for i in range(width):
             x = -2.75 + 4.0 * i / width
-            c = mandel(x, y) % 2
+            c = mandel(complex(x, y)) % 2
             data.append(c)
     print("done")
 
-    with open('mandel.ppm', 'wb') as fo:
+    with open('out.ppm', 'wb') as fo:
         fo.write(b'P4\n')
         fo.write(b'# partable bitmap image of the Mandelbrot set\n')
         fo.write(b'%i %i\n' % (width, height))
