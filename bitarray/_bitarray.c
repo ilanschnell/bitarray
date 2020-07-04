2a5e405728c112f6c550f8b772be6a6e9dde26fa
Ilan Schnell <ilanschnell@gmail.com>
2020-07-03 20:25:42 -0500

    use unsigned char for bitcount lookup

--- bitarray/_bitarray.c
+++ bitarray/_bitarray.c
@@ -425,8 +425,7 @@ bytereverse(bitarrayobject *self)
     }
 }
 
-
-static int bitcount_lookup[256] = {
+static unsigned char bitcount_lookup[256] = {
     0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
     1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
     1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
