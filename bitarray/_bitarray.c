c2d49f1bec7e046d34a21e7ea15f6189e51c474c
Ilan Schnell <ilanschnell@gmail.com>
2020-07-18 14:13:59 -0500

    fix checking for overflow when nbits is close to PY_SSIZE_T_MAX

--- bitarray/_bitarray.c
+++ bitarray/_bitarray.c
@@ -111,14 +111,14 @@ resize(bitarrayobject *self, Py_ssize_t nbits)
     assert(allocated >= size && size == BYTES(self->nbits));
     /* ob_item == NULL implies ob_size == allocated == 0 */
     assert(self->ob_item != NULL || (size == 0 && allocated == 0));
-    /* allocated ==0 implies size == 0 */
+    /* allocated == 0 implies size == 0 */
     assert(allocated != 0 || size == 0);
 
-    if (nbits < 0) {
+    newsize = BYTES(nbits);
+    if (nbits < 0 || BITS(newsize) < 0) {
         PyErr_SetString(PyExc_OverflowError, "bitarray resize");
         return -1;
     }
-    newsize = BYTES(nbits);
 
     if (newsize == size) {
         /* the memory size hasn't changed - bypass everything */
