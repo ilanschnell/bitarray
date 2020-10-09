f70933ceebf374b81dfae30ee074491e5b23de1d
Ilan Schnell <ilanschnell@gmail.com>
2020-10-08 21:23:08 -0500

    add comments to new example

--- examples/huffman/decodetree.py
+++ examples/huffman/decodetree.py
@@ -7,10 +7,11 @@ from bitarray.util import huffman_code
 
 N = 1_000_000
 
-
+# create Huffman code for N symbols
 code = huffman_code({i: random() for i in range(N)})
 print(len(code))
 
+# create the decodetree object
 t0 = time()
 tree = decodetree(code)
 print('decodetree(code):  %9.6f sec' % (time() - t0))
@@ -21,12 +22,15 @@ plain = [randint(0, N - 1) for _ in range(100)]
 a = bitarray()
 a.encode(code, plain)
 
+# decode using the code dictionary
 t0 = time()
 res = a.decode(code)
 print('decode(code):  %9.6f sec' % (time() - t0))
 assert res == plain
 
+# decode using the decodetree
 t0 = time()
 res = a.decode(tree)
 print('decode(tree):  %9.6f sec' % (time() - t0))
 assert res == plain
+assert tree.todict() == code
