ed7fda46e72801cc4abc4fef3922be43772fce30
Ilan Schnell <ilanschnell@gmail.com>
2021-03-13 21:36:39 -0600

    update work on example

--- examples/serialize.py
+++ examples/serialize.py
@@ -1,22 +1,22 @@
 import binascii
 from random import randint
 
-from bitarray import bitarray, bits2bytes
+from bitarray import bitarray
 from bitarray.util import urandom
 
 
 def serialize(a):
     buffer_info = a.buffer_info()
-    return 'bitarray%d%s%s' % (buffer_info[3],
-                               buffer_info[2][0],
-                               binascii.hexlify(a.tobytes()).decode())
+    return 'bty%d%s%s' % (buffer_info[3], buffer_info[2][0],
+                          binascii.hexlify(a.tobytes()).decode())
 
 def deserialize(s):
-    assert s.startswith('bitarray')
+    if not s.startswith('bty'):
+        raise ValueError("expected string starting with 'bty'")
+    unused = int(s[3])
     ed = {'l': 'little', 'b': 'big'}
-    a = bitarray(endian=ed[s[9]])
-    a.frombytes(binascii.unhexlify(s[10:]))
-    unused = int(s[8])
+    a = bitarray(endian=ed[s[4]])
+    a.frombytes(binascii.unhexlify(s[5:]))
     if unused:
         del a[-unused:]
     return a
@@ -25,7 +25,8 @@ def deserialize(s):
 for n in range(1000):
     a = urandom(n, endian=['little', 'big'][randint(0, 1)])
     s = serialize(a)
-    #print(s)
+    if n < 10:
+        print(s)
     b = deserialize(s)
     assert a == b
     assert a.endian() == b.endian()
