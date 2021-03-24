19ce89e505f370e3300392ce3b8f8238f3f99770
Ilan Schnell <ilanschnell@gmail.com>
2021-03-24 00:24:24 -0500

    declare bitcount_lookup const

--- bitarray/bitarray.h
+++ bitarray/bitarray.h
@@ -97,7 +97,7 @@ setunused(bitarrayobject *self)
     return res;
 }
 
-static unsigned char bitcount_lookup[256] = {
+static const unsigned char bitcount_lookup[256] = {
     0,1,1,2,1,2,2,3,1,2,2,3,2,3,3,4,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,
     1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
     1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
