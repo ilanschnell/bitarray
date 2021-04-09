332dde374d06e79ee52223974b2792b2c459b315
Ilan Schnell <ilanschnell@gmail.com>
2021-04-08 20:32:16 -0500

    simplify test

--- bitarray/test_bitarray.py
+++ bitarray/test_bitarray.py
@@ -99,11 +99,10 @@ class Util(object):
             repr(type(a)), "<%s 'bitarray.%s'>" %
             ('class' if is_py3k or b == 'frozenbitarray' else 'type', b))
 
-    def assertBit0(self, x):
-        self.assertEqual(repr(x), '0')
-
-    def assertBit1(self, x):
-        self.assertEqual(repr(x), '1')
+    def assertBitEqual(self, x, y):
+        self.assertTrue(repr(x) in '01')
+        self.assertTrue(repr(y) in '01')
+        self.assertEqual(x, y)
 
     def assertStopIteration(self, it):
         self.assertRaises(StopIteration, next, it)
@@ -533,13 +532,13 @@ class SliceTests(unittest.TestCase, Util):
         a = bitarray()
         self.assertRaises(IndexError, a.__getitem__,  0)
         a.append(True)
-        self.assertBit1(a[0])
-        self.assertBit1(a[-1])
+        self.assertBitEqual(a[0], 1)
+        self.assertBitEqual(a[-1], 1)
         self.assertRaises(IndexError, a.__getitem__,  1)
         self.assertRaises(IndexError, a.__getitem__, -2)
         a.append(False)
-        self.assertBit0(a[1])
-        self.assertBit0(a[-1])
+        self.assertBitEqual(a[1], 0)
+        self.assertBitEqual(a[-1], 0)
         self.assertRaises(IndexError, a.__getitem__,  2)
         self.assertRaises(IndexError, a.__getitem__, -3)
         self.assertRaises(TypeError, a.__getitem__, 1.5)
@@ -859,9 +858,9 @@ class MiscTests(unittest.TestCase, Util):
     def test_iter1(self):
         it = iter(bitarray('011'))
         self.assertIsType(it, 'bitarrayiterator')
-        self.assertBit0(next(it))
-        self.assertBit1(next(it))
-        self.assertBit1(next(it))
+        self.assertBitEqual(next(it), 0)
+        self.assertBitEqual(next(it), 1)
+        self.assertBitEqual(next(it), 1)
         self.assertStopIteration(it)
 
     def test_iter2(self):
