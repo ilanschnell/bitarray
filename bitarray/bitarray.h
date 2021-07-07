e7de9aaf56aa6f39b49ae009e08085c63fcae802
Ilan Schnell <ilanschnell@gmail.com>
2021-07-06 23:12:02 -0500

    next release 2.2.2

--- bitarray/bitarray.h
+++ bitarray/bitarray.h
@@ -4,7 +4,7 @@
 
    Author: Ilan Schnell
 */
-#define BITARRAY_VERSION  "2.2.1"
+#define BITARRAY_VERSION  "2.2.2"
 
 /* .ob_size is the byte count of the buffer, not the number of elements.
    The number of elements (bits) is .nbits. */
