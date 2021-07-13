b3d5c5118f592f8fd735d23a8ca9a35266f34f92
Ilan Schnell <ilanschnell@gmail.com>
2021-07-13 15:17:51 -0500

    add more system information to test output

--- bitarray/test_bitarray.py
+++ bitarray/test_bitarray.py
@@ -3422,8 +3422,10 @@ def run(verbosity=1, repeat=1):
     print('bitarray version: %s' % __version__)
     print('sys.version: %s' % sys.version)
     print('sys.prefix: %s' % sys.prefix)
-    print('pointer size: %d bit' % (8 * _sysinfo()[0]))
-    print('PY_UINT64_T defined: %s' % bool(_sysinfo()[5]))
+    info = _sysinfo()
+    print('pointer size: %d bit' % (8 * info[0]))
+    print('sizeof(size_t): %d' % info[1])
+    print('PY_UINT64_T defined: %s' % info[5])
     suite = unittest.TestSuite()
     for cls in tests:
         for _ in range(repeat):
