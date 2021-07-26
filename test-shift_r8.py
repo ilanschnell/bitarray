1b10903761dcc49334bab9442f599f9b49e3cc17
Ilan Schnell <ilanschnell@gmail.com>
2021-07-25 20:59:53 -0500

    naming

--- test-shift_r8.py
+++ test-shift_r8.py
@@ -8,7 +8,7 @@ from bitarray.test_bitarray import Util
 
 class InternalTests(unittest.TestCase, Util):
 
-    def test_empty(self):
+    def test_shift_r8_empty(self):
         a = bitarray()
         a._shift_r8(0, 0, 3)
         self.assertEqual(a, bitarray())
@@ -18,7 +18,7 @@ class InternalTests(unittest.TestCase, Util):
         a._shift_r8(7, 7, 5)
         self.assertEqual(a, b)
 
-    def test_explicit(self):
+    def test_shift_r8_explicit(self):
         a = bitarray('11000100 01111000 10110101 11101011 11001000')
         b = bitarray(a)
         a._shift_r8(2, 2, 7)
@@ -27,7 +27,7 @@ class InternalTests(unittest.TestCase, Util):
         self.assertEqual(
             a, bitarray('11000100 00000011 11000101 10101111 11001000'))
 
-    def my_shift_r8(self, x, a, b, n):
+    def shift_r8(self, x, a, b, n):
         self.assertTrue(a <= b)
         self.assertTrue(n < 8)
         y = x.tolist()
@@ -36,7 +36,7 @@ class InternalTests(unittest.TestCase, Util):
         self.assertEqual(len(y), len(x))
         return bitarray(y, x.endian())
 
-    def test_random(self):
+    def test_shift_r8_random(self):
         for N in range(1, 100):
             x = urandom(8 * N, self.random_endian())
             cx = x.copy()
@@ -45,7 +45,7 @@ class InternalTests(unittest.TestCase, Util):
             n = randint(0, 7)
             if a <= b:
                 x._shift_r8(a, b, n)
-                self.assertEQUAL(x, self.my_shift_r8(cx, a, b, n))
+                self.assertEQUAL(x, self.shift_r8(cx, a, b, n))
             else:
                 self.assertRaises(ValueError, x._shift_r8, a, b, n)
 
