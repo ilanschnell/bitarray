ca126045d9bec3928b3a7a13aa95510b73eb6837
Ilan Schnell <ilanschnell@gmail.com>
2021-08-06 11:18:28 -0500

    wording

--- bitarray/bitarray.h
+++ bitarray/bitarray.h
@@ -4,7 +4,7 @@
 
    Author: Ilan Schnell
 */
-#define BITARRAY_VERSION  "2.3.0"
+#define BITARRAY_VERSION  "2.2.5"
 
 /* .ob_size is buffer size (in bytes), not the number of elements.
    The number of elements (bits) is .nbits. */
