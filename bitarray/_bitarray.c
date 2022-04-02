7c9232e639f6e5ddeabe9769a15f25fbef9ed0db
Ilan Schnell <ilanschnell@gmail.com>
2022-04-01 23:07:15 -0500

    make code more readable

--- bitarray/_bitarray.c
+++ bitarray/_bitarray.c
@@ -165,6 +165,7 @@ bytereverse(bitarrayobject *self, Py_ssize_t a, Py_ssize_t b)
 {
     static char trans[256];
     static int setup = 0;
+    Py_ssize_t i;
     char *cp;
 
     assert(0 <= a && a <= Py_SIZE(self));
@@ -184,8 +185,11 @@ bytereverse(bitarrayobject *self, Py_ssize_t a, Py_ssize_t b)
         setup = 1;
     }
 
-    for (cp = self->ob_item + a; a < b; a++, cp++)
+    cp = self->ob_item + a;
+    for (i = a; i < b; i++) {
         *cp = trans[(unsigned char) *cp];
+        cp++;
+    }
 }
 
 #ifdef PY_UINT64_T
