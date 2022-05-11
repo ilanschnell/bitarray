21016ad1e7746b8a2754b0b20dd59f0afb6ab8fc
Ilan Schnell <ilanschnell@gmail.com>
2022-05-11 00:28:28 -0500

    refactor pbm example

--- examples/pbm.py
+++ examples/pbm.py
@@ -1,8 +1,9 @@
 from bitarray import bitarray, bits2bytes
 
+
 class PBM: # Portable Bitmap
-    def __init__(self, w=0, h=0):
-        self.size = (w, h)
+    def __init__(self, w = 0, h = 0):
+        self.size = w, h
         self.update()
         self.data = bitarray(self.bits, endian='big')
 
@@ -14,46 +15,44 @@ class PBM: # Portable Bitmap
         self.bits = 8 * self.bytes
 
     def info(self):
-        print('size: %s x %s' % self.size)
-        print('bytes per row: %s' % self.bytes_per_row)
-        print('bits per row: %s' % self.bits_per_row)
+        print('size: %d x %d' % self.size)
+        print('bytes per row: %d' % self.bytes_per_row)
+        print('bits per row: %d' % self.bits_per_row)
         print('bitarray: ' + repr(self.data.buffer_info()))
 
     def clear(self):
         self.data.setall(0)
 
     def save(self, filename):
-        fo = open(filename, 'wb')
-        fo.write(b'P4\n')
-        fo.write(b'# This is a partable bitmap (pbm) file.\n')
-        fo.write(('%i %i\n' % (self.size)).encode())
-        self.data.tofile(fo)
-        fo.close()
+        with open(filename, 'wb') as fo:
+            fo.write(b'P4\n')
+            fo.write(b'# This is a partable bitmap (pbm) file.\n')
+            fo.write(('%d %d\n' % self.size).encode())
+            self.data.tofile(fo)
 
     def load(self, filename):
-        fi = open(filename, 'rb')
-        assert fi.readline().strip() == b'P4'
-        while True:
-            line = fi.readline()
-            if not line.startswith(b'#'):
-                self.size = tuple(map(int, line.split()))
-                break
-        self.update()
-        self.data = bitarray(endian='big')
-        self.data.fromfile(fi)
-        fi.close()
-        assert self.data.buffer_info()[1] == self.bytes
+        with open(filename, 'rb') as fi:
+            assert fi.readline().strip() == b'P4'
+            while True:
+                line = fi.readline()
+                if not line.startswith(b'#'):
+                    self.size = tuple(map(int, line.split()))
+                    break
+            self.update()
+            self.data = bitarray(endian='big')
+            self.data.fromfile(fi)
+            fi.close()
+        assert self.data.nbytes == self.bytes
 
-    def address(self, x, y):
+    def address(self, pt):
+        x, y = pt
         return x + self.bits_per_row * y
 
-    def __getitem__(self, s):
-        x, y = s
-        return self.data[self.address(x, y)]
+    def __getitem__(self, pt):
+        return self.data[self.address(pt)]
 
-    def __setitem__(self, s, val):
-        x, y = s
-        self.data[self.address(x, y)] = val
+    def __setitem__(self, pt, val):
+        self.data[self.address(pt)] = val
 
 
 if __name__ == '__main__':
@@ -72,5 +71,5 @@ if __name__ == '__main__':
 
     # draw a straight line from (490, 10) to (110, 390) on top
     for i in range(381):
-        b[490-i, 10+i] = 1
+        b[490 - i, 10 + i] = 1
     b.save('pic3.ppm')
