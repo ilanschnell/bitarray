7545e9bfaf5f04af4e46b95093c1682efade3acf
Ilan Schnell <ilanschnell@gmail.com>
2022-05-12 00:33:11 -0500

    add example file

---
+++ examples/huffman/efficiency.py
@@ -0,0 +1,31 @@
+"""
+see: https://www.youtube.com/watch?v=umTbivyJoiI
+"""
+import sys
+from math import log2
+from collections import Counter
+
+from bitarray.util import huffman_code
+
+
+def efficiency(freq, code):
+    total = sum(freq.values())  # total frequency
+    H = 0.0                     # entrophy
+    L = 0.0                     # average length
+    for s in freq.keys():
+        p = freq[s] / total     # probability
+        H -= p * log2(p)
+        L += p * len(code[s])
+    print('H =', H)
+    print('L =', L)
+    return H / L                # efficiency
+
+if len(sys.argv) > 1:
+    with open(sys.argv[1], 'rb') as fi:
+        plain = bytearray(fi.read())
+else:
+    plain = b'aaabbbcde'
+
+freq = Counter(plain)
+code = huffman_code(freq)
+print("Efficiency =", efficiency(freq, code))
