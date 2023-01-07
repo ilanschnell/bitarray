4d06bd6557007b270aade32f010dead25ffbce7d
Ilan Schnell <ilanschnell@gmail.com>
2023-01-07 13:50:17 -0600

    add notes

--- examples/sparse.py
+++ examples/sparse.py
@@ -1,39 +1,54 @@
+"""
+Implementation of a sparse bitarray
+
+Internally, we store the value of the first bit, and a list of positions at
+which a bit changes from 1 to 0 or vice versa.  For example:
+
+   bitarray('110011111000')
+
+is represented as:
+
+   first bit: 1
+   changes:   [2, 4, 9, 12]
+"""
 from bisect import bisect
 
 from bitarray import bitarray
 from bitarray.util import intervals
 
 
-class sparse:
+class SparseBitarray:
 
     def __init__(self, a):
-        self.first = a[0] if a else 0
+        self.first = a[0] if a else None
         self.stops = [t[2] for t in intervals(a)]
 
     def __len__(self):
         return self.stops[-1] if self.stops else 0
 
     def __getitem__(self, i):
+        if not 0 <= i < len(self):
+            raise IndexError
         p = bisect(self.stops, i)
         return (self.first + p) % 2
 
-    def intervals(self):
+    def _intervals(self):
         v = self.first
         start = 0
         for stop in self.stops:
-            yield v, start, stop
+            yield int(v), start, stop
             v = not v
             start = stop
 
     def export(self):
         res = bitarray(len(self))
-        for v, start, stop in self.intervals():
+        for v, start, stop in self._intervals():
             res[start:stop] = v
         return res
 
     def count(self, value=1):
         cnt = 0
-        for v, start, stop in self.intervals():
+        for v, start, stop in self._intervals():
             if v == value:
                 cnt += stop - start
         return cnt
@@ -50,20 +65,20 @@ class TestsSparse(unittest.TestCase, Util):
 
     def test_init_export(self):
         for a in self.randombitarrays():
-            s = sparse(a)
+            s = SparseBitarray(a)
             self.assertEqual(len(s), len(a))
             self.assertEqual(s.export(), a)
 
     def test_getitem(self):
         for a in self.randombitarrays(start=1):
-            s = sparse(a)
+            s = SparseBitarray(a)
             for _ in range(10):
                 i = randint(0, len(s) - 1)
                 self.assertEqual(s[i], a[i])
 
     def test_count(self):
         for a in self.randombitarrays():
-            s = sparse(a)
+            s = SparseBitarray(a)
             for v in 0, 1:
                 self.assertEqual(s.count(v), a.count(v))
 
