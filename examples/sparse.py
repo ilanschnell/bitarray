88fcfc09bcee02de8dd0257ea4b1d564f1bfcd14
Ilan Schnell <ilanschnell@gmail.com>
2023-01-08 13:58:18 -0600

    simplify representation in example

--- examples/sparse.py
+++ examples/sparse.py
@@ -1,17 +1,19 @@
 """
 Implementation of a sparse bitarray
 
-Internally, we store the value of the first bit, and a list of positions at
-which a bit changes from 1 to 0 or vice versa.  For example:
+Internally we store a list of positions at a which a bit changes from
+1 to 0 or vice versa.  Moreover, we start with bit 0, meaning that if the
+first bit in the bitarray is 1 our list starts with posistion 0.
+For example:
 
    bitarray('110011111000')
 
 is represented as:
 
-   first bit: 1
-   changes:   [2, 4, 9, 12]
+   changes:   [0, 2, 4, 9, 12]
 """
 from bisect import bisect
+from collections import Counter
 
 from bitarray import bitarray
 from bitarray.util import intervals
@@ -20,24 +22,40 @@ from bitarray.util import intervals
 class SparseBitarray:
 
     def __init__(self, a):
-        self.first = a[0] if a else None
-        self.stops = [t[2] for t in intervals(a)]
+        lst = [] if a and a[0] == 0 else [0]
+        lst.extend(t[2] for t in intervals(a))
+        assert len(lst) > 0
+        self.stops = lst
 
     def __len__(self):
-        return self.stops[-1] if self.stops else 0
+        return self.stops[-1]
 
     def __getitem__(self, i):
         if not 0 <= i < len(self):
             raise IndexError
+        return bisect(self.stops, i) % 2
+
+    def __setitem__(self, i, value):
+        if not 0 <= i < len(self):
+            raise IndexError
         p = bisect(self.stops, i)
-        return (self.first + p) % 2
+        v = p % 2
+        if v == value:
+            return
+        self.stops[p:p] = [i, i + 1]
+        self._reduce()
+
+    def _reduce(self):
+        cnt = Counter(self.stops)
+        cnt[self.stops[-1]] = 1
+        self.stops = sorted(i for i, c in cnt.items() if c % 2)
 
     def _intervals(self):
-        v = self.first
+        v = 0
         start = 0
         for stop in self.stops:
-            yield int(v), start, stop
-            v = not v
+            yield v, start, stop
+            v = 1 - v
             start = stop
 
     def export(self):
@@ -53,6 +71,20 @@ class SparseBitarray:
                 cnt += stop - start
         return cnt
 
+"""
+a = bitarray('110011111000')
+print(a)
+s = SparseBitarray(a)
+print(s.stops)
+i = 11
+for x in range(2):
+    s[i] = a[i] = x % 2
+print(s.stops)
+s._reduce()
+print(s.stops)
+print(s.export())
+print(a)
+"""
 # ---------------------------------------------------------------------------
 
 from random import randint
@@ -75,6 +107,16 @@ class TestsSparse(unittest.TestCase, Util):
             for _ in range(10):
                 i = randint(0, len(s) - 1)
                 self.assertEqual(s[i], a[i])
+
+    def test_setitem(self):
+        for a in self.randombitarrays(start=1):
+            s = SparseBitarray(a)
+            for _ in range(10):
+                i = randint(0, len(s) - 1)
+                v = randint(0, 1)
+                s[i] = a[i] = v
+                self.assertEqual(s.export(), a)
+            #print(s.stops)
 
     def test_count(self):
         for a in self.randombitarrays():
