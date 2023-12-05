15a6eed363cc65cf07b65851f91861990e9c4bef
Ilan Schnell <ilanschnell@gmail.com>
2023-12-04 19:45:19 -0600

    update example

--- examples/shift_r8.c
+++ examples/shift_r8.c
@@ -1,38 +1,20 @@
 /*
-   The purpose of this C program is to illustrate and document shift_r8()
-   and in particular shift_r8le().
-
-   In order take advantage of word shifts, it is necessary that the
-   bit-order of the bitarray object is the same as the native machine
-   byte-order.
+   The purpose of this C program is to illustrate and document shift_r8(),
+   in particular the functions shift_r8le() and shift_r8be().
 
    The function shift_r8() dispatches to shift_r8le() and shift_r8be().
-
-   shift_r8le() assumes the buffer represents a bitarray with little-endian
-   bit-endianness.  It takes advantage of word shifts on little-endian
-   machines.
-
-   shift_r8be() assumes the buffer represents a bitarray with big-endian
-   bit-endianness.  It performs byte shifts only.
-   It would be possible to also take advantage of word shift for big-endian
-   bitarrays on big-endian machines in this function.  However, as big-endian
-   machines are very rare, this is not being done.
-   Another approach would be to have basically the following function body:
-
-       bytereverse(buff, n);
-       shift_r8le(buff, n, k);
-       bytereverse(buff, n);
-
-   While this takes advantage of word shifts of big-endian bitarrays on
-   little-endian machines, it requires two bytereverse() calls.
-   There is no speed improvement of this approach over the current
-   implementation (of performing byte shifts only).
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 
 #define Py_ssize_t  ssize_t
+
+#if (defined(__clang__) || defined(__GNUC__))
+#define IS_GNUC  1
+#else
+#define IS_GNUC  0
+#endif
 
 /* machine byte-order */
 #define PY_LITTLE_ENDIAN  (*((uint64_t *) "\xff\0\0\0\0\0\0\0") == 0xff)
@@ -74,6 +56,42 @@ void shift_r8le(unsigned char *buff, Py_ssize_t n, int k)
     }
 }
 
+/* Shift n bytes in buffer by k bits to right (towards higher addresses),
+   using uint64 (word) shifts when possible.
+
+   As we shift bits right, we need to start with the highest address
+   and loop downwards such that "carry" bytes are still unaltered.
+
+   This function assumes that the buffer represents a bitarray with
+   big-endian bit-endianness.
+*/
+static void
+shift_r8be(unsigned char *buff, Py_ssize_t n, int k)
+{
+    Py_ssize_t w = 0;
+
+    if (PY_LITTLE_ENDIAN && IS_GNUC) {  /* use shift word */
+        w = n / 8;                /* number of words used for shifting */
+        n %= 8;                   /* number of remaining bytes */
+    }
+    while (n--) {                 /* shift bytes (from highest to lowest) */
+        Py_ssize_t i = n + 8 * w;
+        buff[i] >>= k;            /* shift byte */
+        if (n || w)               /* add shifted next lower byte */
+            buff[i] |= buff[i - 1] << (8 - k);
+    }
+#if IS_GNUC
+    while (w--) {                 /* shift in word-range(0, w) */
+        uint64_t *p = ((uint64_t *) buff) + w;
+        *p = __builtin_bswap64(*p);    /* swap bytes in word  */
+        *p >>= k;                      /* shift word */
+        *p = __builtin_bswap64(*p);    /* swap bytes in word */
+        if (w)                    /* add shifted byte from next lower word */
+            buff[8 * w] |= buff[8 * w - 1] << (8 - k);
+    }
+#endif
+}
+
 /* display first nbits bytes of buffer given assumed bit-endianness
    to one line in stdout */
 void display(unsigned char *buffer, Py_ssize_t nbits, int endian)
@@ -96,8 +114,8 @@ int main()
 
     for (i = 0; i < 30; i++) {
         /* Try changing this to ENDIAN_BIG and see what happens! */
-        display(array, 77, ENDIAN_LITTLE);
-        shift_r8le(array, NBYTES, 1);
+        display(array, 77, ENDIAN_BIG);
+        shift_r8be(array, NBYTES, 1);
     }
     return 0;
 }
