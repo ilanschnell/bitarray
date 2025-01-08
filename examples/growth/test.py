50b8eb54763f67e494f2cb3f1a8f612bdf1cc3c6
Ilan Schnell <ilanschnell@gmail.com>
2025-01-07 22:42:19 -0600

    add get_alloc() in example

--- examples/growth/test.py
+++ examples/growth/test.py
@@ -1,6 +1,9 @@
 from bitarray import bitarray
 
 
+def get_alloc(a):
+    return a.buffer_info()[4]
+
 def show(a):
     info = a.buffer_info()
     print('%18d %10d %10d' % (info[0], info[1], info[4]))
@@ -10,7 +13,7 @@ def show(a):
 a = bitarray()
 prev = -1
 while len(a) < 1_000_000:
-    alloc = a.buffer_info()[4]
+    alloc = get_alloc(a)
     assert prev <= alloc
     prev = alloc
     a.append(1)
@@ -19,7 +22,7 @@ while len(a) < 1_000_000:
 # ensure that when we start from a large array and delete part, we always
 # get a decreasing allocation
 a = bitarray(10_000_000)
-prev = a.buffer_info()[4]
+prev = get_alloc(a)
 for _ in range(100):
     del a[-100_000:]
     alloc = a.buffer_info()[4]
@@ -32,20 +35,20 @@ for n in range(1000):
     alloc = (n + 3) & ~3
     assert n <= alloc < n + 4 and alloc % 4 == 0
     a = bitarray(8 * n * [1])
-    assert alloc == a.buffer_info()[4]
+    assert alloc == get_alloc(a)
     b = bitarray(a)
-    assert alloc == b.buffer_info()[4]
+    assert alloc == get_alloc(b)
 
 
 # starting from a large bitarray, make we sure we don't realloc each time
 # we extend
 a = bitarray(1_000_000)  # no overallocation
-assert a.buffer_info()[4] == 125_000
+assert get_alloc(a) == 125_000
 a.extend(bitarray(8))  # overallocation happens here
-alloc = a.buffer_info()[4]
+alloc = get_alloc(a)
 for _ in range(1000):
     a.extend(bitarray(8))
-    assert a.buffer_info()[4] == alloc
+    assert get_alloc(a) == alloc
 
 
 print("OK")
