cea2dbc555a7ef98b698ea43bc1e18463f3fb3e3
Ilan Schnell <ilanschnell@gmail.com>
2025-02-21 00:21:34 -0600

    add code to lfsr example

--- examples/lfsr.py
+++ examples/lfsr.py
@@ -1,10 +1,19 @@
+# Linear Feedback Shift Register
 # https://www.youtube.com/watch?v=Ks1pw1X22y4
 from bitarray import bitarray
+from bitarray.util import parity
+
+state = 0b1001
+for _ in range(20):
+    print(state & 1, end='')
+    newbit = (state ^ (state >> 1)) & 1
+    state = (state >> 1) | (newbit << 3)
+print()
 
 state = bitarray("1001")
-
 for _ in range(20):
-    print(state)
-    newbit = state[-1] ^ state[-2]
-    state >>= 1
-    state[0] = newbit
+    print(state[0], end='')
+    newbit = parity(state[[0, 1]])
+    state.pop(0)
+    state.append(newbit)
+print()
