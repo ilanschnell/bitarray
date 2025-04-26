64fc1b5fc3318617bdcb5ab67477ad2c5a477383
Ilan Schnell <ilanschnell@gmail.com>
2025-04-26 02:47:43 -0500

    extend example

--- examples/internal.py
+++ examples/internal.py
@@ -11,19 +11,25 @@ class InternalTests(unittest.TestCase):
         # used in various places in C code
         for i in range(0, 11):
             for k in range(-10, 2000):
-                res1 = k not in range(0, 1 << i)
-                # simply shift i to right and see if anything remains
-                res2 = bool(k >> i)
+                m = 1 << i
+                res1 = k not in range(0, m)
+                res2 = k < 0 or k >= m
                 self.assertEqual(res1, res2)
+                # simply shift i to right and see if anything remains
+                res3 = bool(k >> i)
+                self.assertEqual(res1, res3)
 
     def test_range_check_2(self):
         # this is used in _util.c in set_count()
         for i in range(0, 11):
             for k in range(-10, 2000):
-                res1 = k not in range(0, (1 << i) + 1)
-                # same as above but combined with k substracted by 1
-                res2 = bool(k >> i) and bool((k - 1) >> i)
+                m = 1 << i
+                res1 = k not in range(0, m + 1)
+                res2 = k < 0 or k > m
                 self.assertEqual(res1, res2)
+                # same as above but combined with k substracted by 1
+                res3 = bool(k >> i) and bool((k - 1) >> i)
+                self.assertEqual(res1, res3)
 
 
 if __name__ == '__main__':
