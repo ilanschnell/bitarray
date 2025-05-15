44ca2195522c702ee8c929f1482b05bf3025b17e
Ilan Schnell <ilanschnell@gmail.com>
2025-05-14 23:58:19 -0500

    test is now also independent of bit-order

--- bitarray/test_bitarray.py
+++ bitarray/test_bitarray.py
@@ -763,27 +763,27 @@ class ZLW_Tests(unittest.TestCase, Util):
 
     def test_zeros(self):
         for n in range(200):
-            a = zeros(n, "little")
+            a = zeros(n, self.random_endian())
             b = _zlw(a)
             self.assertEqual(b, zeros(64))
 
     def test_ones(self):
         for n in range(200):
-            a = ones(n, "little")
+            a = ones(n, self.random_endian())
             b = _zlw(a)
             m = n % 64
-            self.assertEqual(len(b), 64)
             self.assertEqual(b, ones(m) + zeros(64 - m))
-            self.assertEqual(b[63], 0)  # last bit is always 0
 
     def test_random(self):
         for n in range(200):
-            a =  urandom_2(n, "little")
+            a =  urandom_2(n)
             b = _zlw(a)
             nw = 64 * (n // 64)  # bits in complete words
             m = n % 64
             self.assertEqual(len(b), 64)
+            self.assertEqual(a.endian, b.endian)
             self.assertEqual(b, a[nw:nw + m] + zeros(64 - m))
+            self.assertEqual(b[63], 0)  # last bit is always 0
 
 # -------------------------- (Number) index tests ---------------------------
 
