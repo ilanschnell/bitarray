a03c3a7b1a7924511ee01f2e91ed2b32610e6122
Ilan Schnell <ilanschnell@gmail.com>
2025-06-13 01:09:57 -0500

    make code more readable

--- bitarray/_util.c
+++ bitarray/_util.c
@@ -510,10 +510,11 @@ byteswap(PyObject *module, PyObject *args)
     if (PyObject_GetBuffer(buffer, &view, PyBUF_SIMPLE | PyBUF_WRITABLE) < 0)
         return NULL;
 
-    if (n == 0)
-        /* avoid n = 0 as 0 % n fails on some compilers */
-        n = Py_MAX(1, view.len);
-
+    if (n == 0) {
+        n = view.len;
+        if (n == 0)  /* avoid division by zero below */
+            Py_RETURN_NONE;
+    }
     if (n < 1) {
         PyErr_Format(PyExc_ValueError, "positive int expect, got %zd", n);
         goto error;
