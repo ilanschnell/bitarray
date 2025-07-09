7ef0a80de0c0a2116fc1f651d699c77ec36bcf3f
Ilan Schnell <ilanschnell@gmail.com>
2025-07-09 00:16:09 -0500

    use format string in sieve example

--- examples/sieve.py
+++ examples/sieve.py
@@ -27,18 +27,18 @@ print(list(a.search(1, 0, stop)))
 
 # There are 5,761,455 primes up to 100 million
 x = a.count()
-print('there are %d primes up to %d' % (x, N))
+print('there are {:,d} primes up to {:,d}'.format(x, N))
 assert x == 5_761_455 or N != 100_000_000
 
 # The number of twin primes up to 100 million is 440,312
 # we need to add 1 as .count() only counts non-overlapping sub_bitarrays
 # and (3, 5) as well as (5, 7) are both twin primes
 x = a.count(bitarray('101')) + 1
-print('number of twin primes up to %d is %d' % (N, x))
+print('number of twin primes up to {:,d} is {:,d}'.format(N, x))
 assert x == 440_312 or N != 100_000_000
 
 # The 1 millionth prime number is 15,485,863
 m = 1_000_000
 x = count_n(a, m) - 1
-print('the %dth prime is %d' % (m, x))
+print('the {:,d}-th prime is {:,d}'.format(m, x))
 assert x == 15_485_863
