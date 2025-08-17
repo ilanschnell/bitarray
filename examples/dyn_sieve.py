f03cc60f9a1f00c5d258b3ce9c3533ead0d48ee9
Ilan Schnell <ilanschnell@gmail.com>
2025-08-17 17:31:18 -0500

    optimize dynamic sieve example

--- examples/dyn_sieve.py
+++ examples/dyn_sieve.py
@@ -1,4 +1,5 @@
-from math import sqrt
+import math
+import itertools
 
 from bitarray import bitarray
 from bitarray.util import ones, count_n
@@ -10,121 +11,118 @@ class Sieve:
     Eratosthenes.  Similar to prime number sieve in SymPy, but implemented
     using a bitarray.
     """
-    a = ones(210)
-    for i in 2, 3, 5, 7:
-        a[::i] = 0
+    a = ones(105)
+    a[1::3] = 0
+    a[2::5] = 0
+    a[3::7] = 0
 
     def __init__(self):
-        self.data = bitarray(0)
+        self.data = bitarray()
 
-    def __len__(self):
-        return len(self.data)
+    def extend(self, i):
+        "grow to accomodate i, ie. self.data[i//2] will not raise IndexError"
+        if i < 0:
+            raise ValueError("positive integer expected")
 
-    def extend(self, n):
-        n = int(n)
-        if n < 0:
-            raise ValueError("bitarray length must be >= 0")
-
-        if n == 0:  # reset
-            self.data = bitarray(0)
+        if i == 0:  # reset
+            self.data = bitarray()
             return
 
-        if n <= len(self):
+        n = i // 2 + 1  # n is minimal length of self.data
+        if n <= len(self.data):
             return
 
         fresh_data = not self.data
         if fresh_data:
             self.data = self.a.copy()
-            self.data[:8] = bitarray("00110101")
+            self.data[:8] = bitarray("01110110")
 
         a = self.data
         n1 = len(a)
-        m = (n - n1 + 210 - 1) // 210
-        self.data += m * self.a
+        m = (n - n1 + 105 - 1) // 105
+        assert fresh_data or m > 0
+        a += m * self.a
         if fresh_data:
-            n1 = 121
-        for i in a.search(1, 11, int(sqrt(len(a)) + 1.0)):
-            i2 = i * i
-            k = (i2 - n1) % i + n1 if i2 < n1 else i2
+            n1 = 60
+        for i in a.search(1, 5, int(math.sqrt(len(a) // 2) + 1.0)):
+            j = 2 * i + 1
+            j2 = (j * j) // 2
+            k = (j2 - n1) % j + n1 if j2 < n1 else j2
             assert k >= n1
-            a[k :: i] = 0
+            a[k :: j] = 0
 
     def extend_to_no(self, n):
-        self.extend(1)
-        while self.data.count() < n:
-            self.extend(int(len(self) * 1.5))
+        while self.data.count() + 1 < n:
+            self.extend(3 * len(self.data) + 1)
 
-    def __contains__(self, p):
-        if p < 0:
+    def __contains__(self, i):
+        if i < 0:
             raise ValueError("positive integer expected")
-        self.extend(p + 1)
-        return self.data[p]
+        if i % 2 == 0:
+            return i == 2
+        self.extend(i)
+        return self.data[i // 2]
 
     def __iter__(self):
-        i = 1
-        while True:
-            self.extend(i + 1)
-            if self.data[i]:
+        yield 2
+        for i in itertools.count(start=3, step=2):
+            self.extend(i)
+            if self.data[i // 2]:
                 yield i
-            i += 1
 
     def __getitem__(self, n):
         "return n-th prime"
-        if isinstance(n, slice):
-            self.extend_to_no(n.stop)
-            start = n.start if n.start is not None else 0
-            if start < 1:
-                # sieve[:5] would be empty (starting at -1), let's
-                # just be explicit and raise
-                raise IndexError("Sieve indices start at 1.")
-            a = count_n(self.data, start) - 1
-            b = count_n(self.data, n.stop) - 1
-            lst = list(self.primerange(a, b))
-            return lst[::n.step]
-
         if n < 1:
             # offset is one, so forbid explicit access to sieve[0]
             raise IndexError("Sieve indices start at 1")
+        if n == 1:
+            return 2
         self.extend_to_no(n)
-        return count_n(self.data, n) - 1
-
-    def primerange(self, a, b):
-        self.extend(b + 1)
-        yield from self.data.search(1, a, b)
+        i = count_n(self.data, n - 1) - 1
+        assert self.data[i]
+        return 2 * i + 1
 
 # ---------------------------------------------------------------------------
 
 import unittest
-from collections import Counter
-from itertools import islice, pairwise
 from random import randrange
 
 from bitarray.util import gen_primes
 
 
-N = 12_000_000
+N = 1_000_000
 PRIMES = gen_primes(N)
+ODD_PRIMES = PRIMES[1::2]
 
 
 class SieveTests(unittest.TestCase):
 
-    def check_data(self, s, n):
-        if n == 0:
-            self.assertEqual(len(s), 0)
+    def check_data(self, s, i):
+        if i == 0:
+            self.assertEqual(len(s.data), 0)
             return
-        if n <= len(s):
-            n = len(s)
-        n = 210 * ((n + 210 - 1) // 210)
-        self.assertEqual(len(s), n)
-        self.assertEqual(s.data, PRIMES[:n])
+        n = i // 2 + 1
+        if n <= len(s.data):
+            n = len(s.data)
+        n = 105 * ((n + 105 - 1) // 105)
+        self.assertEqual(len(s.data), n)
+        self.assertEqual(s.data, ODD_PRIMES[:n])
 
     def test_random(self):
         s = Sieve()
         for _ in range(1000):
-            n = randrange(1000) if randrange(10) else 0
-            s.extend(n)
-            self.check_data(s, n)
-            #print(n, len(s))
+            i = randrange(1000) if randrange(10) else 0
+            s.extend(i)
+            self.check_data(s, i)
+            #print(n, len(s.data))
+
+    def test_contains(self):
+        s = Sieve()
+        for i, v in enumerate(PRIMES[:1000]):
+            self.assertEqual(i in s, v)
+        for _ in range(1000):
+            i = randrange(1_000_000)
+            self.assertEqual(i in s, PRIMES[i])
 
     def test_iter(self):
         s = Sieve()
@@ -134,59 +132,15 @@ class SieveTests(unittest.TestCase):
                 break
             a.append(i)
         self.assertEqual(a[-1], 997)
-        self.assertEqual(a, list(s.primerange(0, 1000)))
+        self.assertEqual(a, list(PRIMES.search(1, 0, 1000)))
 
-    def test_islice(self):
+    def test_getitem(self):
         s = Sieve()
-        it = islice(s, 3, 10)
-        self.assertEqual(list(it), [7, 11, 13, 17, 19, 23, 29])
-        it = islice(s, 168)
-        self.assertEqual(sum(it), 76_127)
-
-    def test_pairwiase(self):
-        s = Sieve()
-        C = Counter()
-        for a, b in pairwise(s):
-            C[b - a] += 1
-            if b > 540:
-                break
-        self.assertEqual(C[1],  1) # 3 - 2
-        self.assertEqual(C[2], 25) # twins
-        self.assertEqual(C[4], 26) # cousin
-        self.assertEqual(C[6], 25) # sexy
-
-    def test_is_prime(self):
-        s = Sieve()
-        for i, v in enumerate(PRIMES[:1000]):
-            self.assertEqual(i in s, v)
-        for _ in range(1000):
-            i = randrange(1_000_000)
-            self.assertEqual(i in s, PRIMES[i])
-
-    def test_ith_prime(self):
-        s = Sieve()
+        self.assertEqual(s[1], 2)
+        self.assertEqual(s[2], 3)
+        self.assertEqual(s[3], 5)
         self.assertEqual(s[10], 29)
         self.assertEqual(s[1_000_000], 15_485_863)
-        self.assertEqual(s[10:12], [29, 31])
-        self.assertEqual(s[1:12], [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31])
-        self.assertEqual(s[2:7], [3, 5, 7, 11, 13])
-        self.assertEqual(s[2:7:2], [3, 7, 13])
-
-    def test_primerange(self):
-        s = Sieve()
-        self.assertEqual(list(s.primerange(7, 19)), [7, 11, 13, 17])
-        self.assertEqual(list(s.primerange(80, 100)), [83, 89, 97])
-
-    def test_count(self):
-        s = Sieve()
-        for n, res in [
-                (    10,    4),
-                (   100,   25),
-                ( 1_000,  168),
-                (10_000, 1229),
-        ]:
-            s.extend(n)
-            self.assertEqual(s.data.count(1, 0, n), res)
 
 
 if __name__ == '__main__':
