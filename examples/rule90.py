fb600342b3ceccb9a198667629dbdcb008fb4a6d
Ilan Schnell <ilanschnell@gmail.com>
2026-07-09 09:21:13 -0500

    add example

---
+++ examples/rule90.py
@@ -0,0 +1,20 @@
+import time
+from bitarray.util import zeros
+
+# https://en.wikipedia.org/wiki/Rule_90
+# 1-D cellular automaton with periodic boundary conditions
+def rule90(a):
+    left = a.copy()
+    right = a.copy()
+    left.rotate(-1)
+    right.rotate(1)
+    return left ^ right
+
+
+state = zeros(79)
+state[35] = 1
+
+while 1:
+    print(state.unpack(zero=b' ', one=b'#').decode())
+    state = rule90(state)
+    time.sleep(0.02)
