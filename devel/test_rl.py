0b4d6b81a7789c6b1a15fd143a7d3e64ec03b9f0
Ilan Schnell <ilanschnell@gmail.com>
2026-08-15 02:54:36 -0500

    expand tests for internal LEB128 functions

--- devel/test_rl.py
+++ devel/test_rl.py
@@ -1,3 +1,4 @@
+import sys
 import unittest
 from random import randrange
 
@@ -6,10 +7,54 @@ from bitarray._util import leb128_encode, leb128_decode
 
 class LEB128_Tests(unittest.TestCase):
 
-    def test_example(self):
-        i = 624485
-        b = [0xe5, 0x8e, 0x26]
-        self.assertEqual(leb128_encode(i), bytes(b))
+    def test_explicit(self):
+        for i, b in [
+                (      0, b'\x00'),
+                (    127, b'\x7f'),
+                (    128, b'\x80\x01'),
+                ( 16_383, b'\xff\x7f'),
+                ( 16_384, b'\x80\x80\x01'),
+                (624_485, b'\xe5\x8e\x26'),  # wikipedia LEB128
+        ]:
+            self.assertEqual(leb128_encode(i), b)
+            self.assertEqual(leb128_decode(b), i)
+            it = iter(b)
+            self.assertEqual(leb128_decode(it), i)
+            self.assertRaises(StopIteration, next, it)
+            it = iter(b + b'XYZ')
+            self.assertEqual(leb128_decode(it), i)
+            self.assertEqual(next(it), ord(b'X'))
+
+    def test_encode_errors(self):
+        E = leb128_encode
+        self.assertRaises(ValueError, E, -1)
+        self.assertRaises(ValueError, E, -123)
+        self.assertRaises(OverflowError, E, sys.maxsize + 1)
+        self.assertRaises(OverflowError, E, -sys.maxsize - 2)
+        self.assertRaises(TypeError, E, 1.0)
+
+    def test_decode_types(self):
+        lst = [0xe5, 0x8e, 0x26]
+        for b in lst, bytes(lst), bytearray(lst), iter(lst):
+            self.assertEqual(leb128_decode(b), 624_485)
+
+    def test_decode_errors(self):
+        D = leb128_decode
+        self.assertRaises(ValueError, D, [-1])
+        self.assertRaises(ValueError, D, [0xff, 256])
+        self.assertRaises(TypeError, D, "String")
+        for b in b'', b'\xff', b'\xff\xff':
+            self.assertRaises(StopIteration, D, b)
+        self.assertRaises(OverflowError, D, 9 * b'\xff')
+
+    def test_maxsize(self):
+        i = sys.maxsize
+        width = i.bit_length() + 1
+        if width == 64:
+            b = 8 * b'\xff' + b'\x7f'
+        elif width == 32:
+            b = 4 * b'\xff' + b'\x07'
+        self.assertEqual(leb128_encode(i), b)
         self.assertEqual(leb128_decode(b), i)
 
     def test_range(self):
